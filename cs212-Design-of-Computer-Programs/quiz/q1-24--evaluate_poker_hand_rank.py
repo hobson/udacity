743d2045017a8ff205f7d083b85929c47cd9e5b8
Hobson Lane <hobsonlane@gmail.com>
2013-05-25 23:00:50 -0700

    poker tests pass

--- cs212-Design-of-Computer-Programs/quiz/q1-24--evaluate_poker_hand_rank.py
+++ cs212-Design-of-Computer-Programs/quiz/q1-24--evaluate_poker_hand_rank.py
@@ -1,48 +1,31 @@
 #!/usr/bin/env python
 # -----------
 # User Instructions
-# 
+#
 # Write a function, allmax(iterable, key=None), that returns
-# a list of all items equal to the max of the iterable, 
-# according to the function specified by key. 
+# a list of all items equal to the max of the iterable,
+# according to the function specified by key.
+
 
 def poker(hands):
     "Return a list of winning hands: poker([hand,...]) => [hand,...]"
     return allmax(hands, key=hand_rank)
 
-    def allmax(iterable, key=None):
-        "Return a list of all items equal to the max of the iterable."
-        # Your code here.
-        # from Norvig:
-        #result, maxval = [], None
-        key = key or (lambda x:x)
-        #for x in iterable:
-        #    xval = key(x)
-        #    if not result or xval > maxval:
-        #        result, maxval = [x], xval
-        #    elif xval == maxval:
-        #        result.append(x)
-        #return result
-        # if you don't make a new list, the iterable will be consumed to the end!
-        #print mx
-        #print hand_rank(mx)
-        #print iterable
-        #print [x for x in l]
-        #print [x==mx for x in l]
-        #print map(lambda x:(hand_rank(x)==hand_rank(mx)), iter(l))
-        #print map(hand_rank, iter(l))
-        L = list(iterable)
-        # so now l holds the data and iterable is empty!
-        import itertools
-        mx = hand_rank(max(L, key=key))
-        return list( itertools.ifilter(lambda x:hand_rank(x)==mx, iter(L) ) )
+
+def allmax(iterable, key=None):
+    "Return a list of all items equal to the max of the iterable."
+    L = list(iterable)
+    import itertools
+    mx = max(key(x) for x in L)
+    return list(itertools.ifilter(lambda x: key(x) == mx, iter(L)))
+
 
 def hand_rank(hand):
     "Return a value indicating the ranking of a hand."
-    ranks = card_ranks(hand) 
-    if straight(ranks) and flush(hand):
+    ranks = card_ranks(hand)
+    if straight(ranks) and flush(hand):  # straight flush
         return (8, max(ranks))
-    elif kind(4, ranks):
+    elif kind(4, ranks):  # four of a kind
         return (7, kind(4, ranks), kind(1, ranks))
     elif kind(3, ranks) and kind(2, ranks):
         return (6, kind(3, ranks), kind(2, ranks))
@@ -59,27 +42,33 @@ def hand_rank(hand):
     else:
         return (0, ranks)
 
+
 def card_ranks(hand):
     "Return a list of the ranks, sorted with higher first."
     ranks = ['--23456789TJQKA'.index(r) for r, s in hand]
-    ranks.sort(reverse = True)
-    return [5, 4, 3, 2, 1] if (ranks == [14, 5, 4, 3, 2]) else ranks
+    ranks.sort(reverse=True)
+    return ranks
+
 
 def flush(hand):
     "Return True if all the cards have the same suit."
-    suits = [s for r,s in hand]
+    suits = (s for r, s in hand)
     return len(set(suits)) == 1
+
 
 def straight(ranks):
     "Return True if the ordered ranks form a 5-card straight."
     return (max(ranks)-min(ranks) == 4) and len(set(ranks)) == 5
 
+
 def kind(n, ranks):
     """Return the first rank that this hand has exactly n-of-a-kind of.
     Return None if there is no n-of-a-kind in the hand."""
     for r in ranks:
-        if ranks.count(r) == n: return r
+        if ranks.count(r) == n:
+            return r
     return None
+
 
 def two_pair(ranks):
     "If there are two pair here, return the two ranks of the two pairs, else None."
@@ -90,15 +79,42 @@ def two_pair(ranks):
     else:
         return None
 
+
 def test():
-    "Test cases for the functions in poker program."
-    sf1 = "6C 7C 8C 9C TC".split() # Straight Flush
-    sf2 = "6D 7D 8D 9D TD".split() # Straight Flush
-    fk = "9D 9H 9S 9C 7D".split() # Four of a Kind
-    fh = "TD TC TH 7C 7D".split() # Full House
-    assert poker([sf1, sf2, fk, fh]) == [sf1, sf2] 
+    "Test cases for the functions in poker program"
+    sf = "6C 7C 8C 9C TC".split()  # Straight Flush
+    fk = "9D 9H 9S 9C 7D".split()  # Four of a Kind
+    fh = "TD TC TH 7C 7D".split()  # Full House
+    tp = "5H 5D 9C 9H 6S".split()  # two pair
+    assert flush(sf)
+    assert flush(fk) is False
+    assert flush(fh) is False
+    fkranks = card_ranks(fk)
+    tpranks = card_ranks(tp)
+    assert kind(4, fkranks) == 9
+    assert kind(3, fkranks) == None
+    assert kind(2, fkranks) == None
+    assert kind(1, fkranks) == 7
+    assert card_ranks(sf) == [10, 9, 8, 7, 6]
+    assert fkranks == [9, 9, 9, 9, 7]
+    assert card_ranks(fh) == [10, 10, 10, 7, 7]
+
+    assert straight([9, 8, 7, 6, 5]) == True
+    assert straight([9, 8, 8, 6, 5]) == False
+    assert hand_rank(sf) == (8, 10)
+    assert hand_rank(fk) == (7, 9, 7)
+    assert hand_rank(fh) == (6, 10, 7)
+    assert poker([sf, fk, fh]) == [sf]
+    assert poker([fk, fh]) == [fk]
+    assert poker([fh, fh]) == [fh, fh]
+    assert poker([sf]) == [sf]
+    assert poker([sf] + 99*[fh]) == [sf]
+    assert hand_rank(sf) == (8, 10)
+    assert hand_rank(fk) == (7, 9, 7)
+    assert hand_rank(fh) == (6, 10, 7)
+
     return 'tests pass'
 
-print allmax(iter([['6C', '7C', '8C', '9C', 'TC'], ['6D', '7D', '8D', '9D', 'TD'], ['9D', '9H', '9S', '9C', '7D'], ['TD', 'TC', 'TH', '7C', '7D']]),hand_rank)
-assert allmax([['6C', '7C', '8C', '9C', 'TC'], ['6D', '7D', '8D', '9D', 'TD'], ['9D', '9H', '9S', '9C', '7D'], ['TD', 'TC', 'TH', '7C', '7D']],hand_rank) == [['6C', '7C', '8C', '9C', 'TC'],['6D', '7D', '8D', '9D', 'TD']]
 
+#print allmax(iter([['6C', '7C', '8C', '9C', 'TC'], ['6D', '7D', '8D', '9D', 'TD'], ['9D', '9H', '9S', '9C', '7D'], ['TD', 'TC', 'TH', '7C', '7D']]),hand_rank)
+#assert allmax([['6C', '7C', '8C', '9C', 'TC'], ['6D', '7D', '8D', '9D', 'TD'], ['9D', '9H', '9S', '9C', '7D'], ['TD', 'TC', 'TH', '7C', '7D']],hand_rank) == [['6C', '7C', '8C', '9C', 'TC'],['6D', '7D', '8D', '9D', 'TD']]
