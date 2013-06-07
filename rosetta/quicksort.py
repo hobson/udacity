2f099c46840d238ea15bd47150d33a63a68dfbea
Hobson Lane <hobsonlane@gmail.com>
2013-06-06 19:09:34 -0700

    rosetta/quicksort.py

--- rosetta/quicksort.py
+++ rosetta/quicksort.py
@@ -34,22 +34,22 @@ def qs(L):
     >>> qs([4, 10, 2, -7, 0, 12, 5, 25, -1 ,-1])
     [-7, -1, -1, 0, 2, 4, 5, 10, 12, 25]
     """
-    return (qs([x for x in L[1:] if x <  L[0]]) + 
-            L[:1] + 
+    return (qs([x for x in L[1:] if x < L[0]]) +
+            L[:1] +
             qs([x for x in L[1:] if x >= L[0]])) if len(L) > 1 else L
 
 
-def qsort(list):
+def qsort(L):
     """
     Haskellish, but a bit more explicit
 
     >>> qsort([4, 10, 2, -7, 0, 12, 5, 25, -1 ,-1])
     [-7, -1, -1, 0, 2, 4, 5, 10, 12, 25]
     """
-    if not list:
+    if not L:
         return []
     else:
-        pivot = list[0]
-        less = [x for x in list     if x <  pivot]
-        more = [x for x in list[1:] if x >= pivot]
+        pivot = L[0]
+        less = [x for x in L[1:] if x < pivot]
+        more = [x for x in L[1:] if x >= pivot]
         return qsort(less) + [pivot] + qsort(more)
